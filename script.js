80a7361e96c39fa37a5f529e16d60af2d8b48ae0
David Schultz <dschultz@arthrex.com>
2019-11-13 17:35:58 -0600

    basic functionality with centered view

---
+++ script.js
@@ -0,0 +1,17 @@
+const constraints = {
+  audio: false,
+  video: true
+};
+
+navigator.mediaDevices
+  .getUserMedia(constraints)
+  .then(function(mediaStream) {
+    var video = document.querySelector('video');
+    video.srcObject = mediaStream;
+    video.onloadedmetadata = function(e) {
+      video.play();
+    };
+  })
+  .catch(function(err) {
+    console.log(err.name + ': ' + err.message);
+  });
